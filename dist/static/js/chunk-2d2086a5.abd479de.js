53b91eed66446c8426f62e68cb3f131ce242182d
thanhnc <thanhnc138@mail.com>
2021-06-15 06:35:52 +0700

    add static files

---
+++ dist/static/js/chunk-2d2086a5.abd479de.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2086a5"],{a561:function(n,t,a){"use strict";a.r(t);var e=function(n,t){var a=t._c;return a("div",{staticStyle:{padding:"30px"}},[a("el-alert",{attrs:{closable:!1,title:"menu 1-2-2",type:"warning"}})],1)},l=[],u=a("2877"),c={},i=Object(u["a"])(c,e,l,!0,null,null,null);t["default"]=i.exports}}]);
