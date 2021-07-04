32b0260ba353511c8312d2bcf42d725f328770b8
thanhnc <thanhnc138@mail.com>
2021-07-04 22:13:28 +0700

    add Envelope Strategy

--- src/views/backtest/Backtest.vue
+++ src/views/backtest/Backtest.vue
@@ -87,6 +87,16 @@
           </div>
         </div>
         <!-- end RSIStrategy -->
+        <!-- EnvelopeStrategy -->
+        <div v-if="strategy.id === 'EnvelopeStrategy'" class="stategy-parameters">
+          <div class="row">
+            <div class="param-label">Percentage</div>
+            <base-input-number
+              v-model="strategy_params.perc"
+            ></base-input-number>
+          </div>
+        </div>
+        <!-- end EnvelopeStrategy -->
         <!-- UltimateOscillatorStrategy -->
         <div
           v-if="strategy.id === 'UltimateOscillatorStrategy'"
@@ -527,6 +537,11 @@ export default {
         description:
           "Ultimate Oscillator Strategy will execute a buy transaction when Ultimate Oscillator goes oversold and a sell transaction when Ultimate Oscillator goes overbought",
       },
+      {
+        id: "EnvelopeStrategy",
+        label: "Envelope Strategy",
+        description: "Envelope Strategy will execute a buy transaction when he price moved below the lower range and a sell transaction when when the price moved beyond the upper range",
+      },
       //#endregion DONE
       // {
       //   id: "AccelerationDecelerationOscillatorStrategy",
@@ -536,11 +551,6 @@ export default {
       // {
       //   id: "AwesomeOscillatorStrategy",
       //   label: "Awesome Oscillator Strategy",
-      //   // description: "Commodity Channel Index Strategy will execute a buy transaction when fast EMA goes above slow EMA and a sell transaction when fast EMA goes below slow EMA",
-      // },
-      // {
-      //   id: "EnvelopeStrategy",
-      //   label: "Envelope Strategy",
       //   // description: "Commodity Channel Index Strategy will execute a buy transaction when fast EMA goes above slow EMA and a sell transaction when fast EMA goes below slow EMA",
       // },
       // {
@@ -651,6 +661,11 @@ export default {
             period: 14,
             upper: 70,
             lower: 30,
+          };
+          break;
+        case "EnvelopeStrategy":
+          this.strategy_params = {
+            perc: 10,
           };
           break;
         case "UltimateOscillatorStrategy":
